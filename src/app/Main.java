a2649e94527b46e772827e7fbce14cde7637be90
EdAlekseiev <e.alekseiev@gmail.com>
2024-08-22 23:49:23 +0300

    Init commit
    - Add Main class
    - Add logs about project

---
+++ src/app/Main.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Converter Temperature");
+        System.out.println("Version 1.0");
+    }
+}
