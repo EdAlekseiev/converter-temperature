6e90da40aaf2328799837982ce041059e0fad5f5
EdAlekseiev <e.alekseiev@gmail.com>
2024-08-22 23:50:48 +0300

    Add fahrenheit to celsius converter
    - Add basic constants for converter
    - Add method fahrenheitToCelsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,20 @@
 package app;
 
 public class Main {
+    public static final double CONV_K = 1.8;
+    public static final double FAHRENHEIT_SCALE = 32.0;
+
     public static void main(String[] args) {
         System.out.println("Converter Temperature");
-        System.out.println("Version 1.0");
+        System.out.println("Version 1.1");
+
+        double fahrenheits = 75;
+        double celsius = fahrenheitToCelsius(fahrenheits);
+
+        System.out.println(fahrenheits + " fahrenheits are " + celsius + " celsius.");
+    }
+
+    private static double fahrenheitToCelsius(double fahrenheits) {
+        return (fahrenheits - FAHRENHEIT_SCALE) / CONV_K;
     }
 }
